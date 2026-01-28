70d41112923f276a68acfb01a864a0607adda27d
Sangitha Karunanithi <sangiekaruna07@gmail.com>
2026-01-28 22:44:41 +0530

    momentoDesignPAttern

--- DesignPatterns/src/MomentoDesignPattern/Pojo_Snapshot_AccountMemento.java
+++ DesignPatterns/src/MomentoDesignPattern/Pojo_Snapshot_AccountMemento.java
@@ -1,13 +1,13 @@
 package MomentoDesignPattern;
 
 //Snapshot of account state - POJO class
-public class AccountMemento {
+public class Pojo_Snapshot_AccountMemento {
 
     private final  int balance; // note : final
 //final is used to make the memento immutable, so the saved state cannot be changed after creation.
 //If the snapshot itself can change ❌, rollback becomes unreliable.. so final, and no setter
 
-    public AccountMemento(int balance) {
+    public Pojo_Snapshot_AccountMemento(int balance) {
         this.balance = balance;
     }
 
